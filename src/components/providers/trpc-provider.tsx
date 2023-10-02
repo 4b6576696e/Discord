8fb4ee60c46a97917d1a7552c8cfd9e2c53f5244
kevin <kevin@pop-os.localdomain>
2023-10-02 16:45:51 +0530

    final

--- src/components/providers/trpc-provider.tsx
+++ src/components/providers/trpc-provider.tsx
@@ -23,8 +23,8 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode; }> = ({
     );
     // const queryClient = qc;
 
-    const url = process.env.NEXT_PUBLIC_VERCEL_URL
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    const url = process.env.NEXT_PUBLIC_SITE_URL
+        ? `https://${process.env.NEXT_PUBLIC_SITE_URL}`
         : "http://localhost:3000/api/trpc/";
 
     const [trpcClient] = useState(() =>
