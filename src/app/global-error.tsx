e470f0dccbcc22e697cff4e3aebf10fe5783783f
KEVIN <85786718+4b6576696e@users.noreply.github.com>
2023-10-02 18:16:35 +0530

    Update global-error.tsx

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -7,17 +7,28 @@ export default function GlobalError({
   error: Error
   reset: () => void
 }) {
- console.log(error);
-      console.log(error.message);
-    console.log(error.cause);
-    console.log(error.name);
-    console.log(error.stack);
+
+ if (error.message === "Error in body stream") 
+   return <html>
+      <body className="bg-white dark:bg-[#313338]">
+        <div className="flex flex-col flex-1 justify-center items-center h-full">
+            <Loader className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+            <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                Loading...
+            </p>
+        </div>
+      </body>
+    </html>
   
   return (
     <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+      <body className="bg-white dark:bg-[#313338]">
+       <div className="flex flex-col flex-1 justify-center items-center h-full">
+           <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+            <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                Something went wrong!
+            </p>
+        </div>
       </body>
     </html>
   )
