c30484d2f27011c632db7e8ce9f4273e3620e510
KEVIN <85786718+4b6576696e@users.noreply.github.com>
2023-10-02 18:23:04 +0530

    Update global-error.tsx

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,35 +1,38 @@
-'use client'
- 
+'use client';
+
+import { Loader, ServerCrash } from "lucide-react";
+
+
 export default function GlobalError({
-  error,
-  reset,
+    error,
+    reset,
 }: {
-  error: Error
-  reset: () => void
+    error: Error;
+    reset: () => void;
 }) {
 
- if (error.message === "Error in body stream") 
-   return <html>
-      <body className="bg-white dark:bg-[#313338]">
-        <div className="flex flex-col flex-1 justify-center items-center h-full">
-            <Loader className="h-7 w-7 text-zinc-500 animate-spin my-4" />
-            <p className="text-xs text-zinc-500 dark:text-zinc-400">
-                Loading...
-            </p>
-        </div>
-      </body>
-    </html>
-  
-  return (
-    <html>
-      <body className="bg-white dark:bg-[#313338]">
-       <div className="flex flex-col flex-1 justify-center items-center h-full">
-           <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
-            <p className="text-xs text-zinc-500 dark:text-zinc-400">
-                Something went wrong!
-            </p>
-        </div>
-      </body>
-    </html>
-  )
+    if (error.message === "Error in body stream")
+        return <html>
+            <body className="bg-white dark:bg-[#313338]">
+                <div className="flex flex-col flex-1 justify-center items-center h-full">
+                    <Loader className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+                    <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                        Loading...
+                    </p>
+                </div>
+            </body>
+        </html>;
+
+    return (
+        <html>
+            <body className="bg-white dark:bg-[#313338]">
+                <div className="flex flex-col flex-1 justify-center items-center h-full">
+                    <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+                    <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                        Something went wrong!
+                    </p>
+                </div>
+            </body>
+        </html>
+    );
 }
